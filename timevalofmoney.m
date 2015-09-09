01310089392273bdd672b16014136f4121d1c9af
smarshall <smarshall@allegromicro.com>
2015-09-09 16:02:25 -0400

    independent investment amount supported

--- timevalofmoney.m
+++ timevalofmoney.m
@@ -12,7 +12,7 @@ rate2 = 0.057;
 
 
 payment = 50; % monthly payment you wish to make
-invpay = 0; % set a monthly amount if you want to invest right away instead
+invpay = 50; % set a monthly amount if you want to invest right away instead
             % of placing 100% of available 'payment' into debts.
             % if 0, the program assumes all extra payment goes to debt
             % repayment first.
@@ -27,14 +27,14 @@ minpay1 = (-principal1*(rate1/n))/(1-(1+rate1/n)^(-n*horizon));
 minpay2 = (-principal2*(rate2/n))/(1-(1+rate1/n)^(-n*horizon));
 minpay = minpay1+minpay2;
 
-while payment < minpay
-    payment = input(['Your min payment is too small! Increase it to at least ' num2str(ceil(minpay)) ': ']);
+while payment-invpay < minpay
+    payment = input(['Your min payment is too small! Increase it to at least ' num2str(ceil(minpay)+invpay) ': ']);
 end
 %
 
 % determine any extra amount of money beyond the minimums
 if payment > minpay
-    extra = payment-minpay;
+    extra = payment-invpay-minpay;
 else
     extra = 0;
 end
@@ -49,17 +49,19 @@ else
     payment2 = minpay2+extra;
 end
 
-if payment~=(payment1+payment2)
+if payment~=(payment1+payment2+invpay)
     error('borked some math')
 end
 
 % Initialize while loop
 clear presval1
 clear presval2
+clear asset1
 presval1(1) = principal1;
 presval2(1) = principal2;
+asset1(1) = 0;
 networth(1) = principal1+principal2;
-stopval = 000000;
+stopval = 10000;
 period = 0; % initial period
 t=1; % # of periods per cycle of while loop
 
@@ -109,7 +111,16 @@ while networth(period+1) < stopval
         error('You will never pay this off! Use a higher Min Payment!')
     end
     
-    networth(period+2) = presval2(period+2)+presval1(period+2);
+    if presval2(period+2)==0&&presval1(period+2)==0
+        payment1=0;
+        payment2=0;
+    end
+    
+    % asset 1
+        asset1(period+2) = asset1(period+1)+invpay+(payment-(payment1+payment2));
+    
+    
+    networth(period+2) = presval2(period+2)+presval1(period+2)+asset1(period+2);
     period = period+1;
     
 end
@@ -119,4 +130,5 @@ hold on
 plot(presval1,'LineStyle','--')
 hold on
 plot(presval2,'LineStyle','--')
+plot(asset1,'LineStyle','-.')
 plot(networth)
